67f0ba6af1b8570802415b5bc1658568c3dc2830
luoluo <luoluo1920@gmail.com>
2014-05-22 05:32:19 -0700

    core info collect

---
+++ coreInfoCollector.py
@@ -0,0 +1,67 @@
+from myConfig import MyConfig
+class CoreInfoCollector:
+    def __init__(self, inputFile):
+        self.infoDescriptions = []
+        self.inputFile = inputFile
+
+    def collect(self):
+        for infoDescription in self.infoDescriptions: 
+            self.collectOne(infoDescription)
+
+    def collectOne(self, infoDescription):
+        allInfos = open(self.inputFile)
+        dict = {}
+        field = infoDescription.getField()
+        type = infoDescription.getType()
+        for info in allInfos:
+            infoList = info.strip().split("\t")
+            infoToCollect = infoList[field]
+            if type == 3:
+               infoToCollect = infoToCollect.split()[1]
+            elif type == 4:
+               infoToCollect = infoToCollect.split()[0]
+            else:
+                pass
+            if infoToCollect in dict:
+                dict[infoToCollect] += 1
+            else:
+                dict[infoToCollect] = 1
+        print dict
+        return dict
+
+    def collectTime(self, infoDescription):
+        pass
+
+    def collectDate(self, infoDescription):
+        pass
+
+    def loadDescription(self):
+        self.infoDescriptions.append(InfoDescription(0, 0))
+        self.infoDescriptions.append(InfoDescription(1, 1))
+
+    def loadDescriptionFromFIle(self, fileName):
+        myConfig = MyConfig(fileName)
+        descriptions = myConfig.getListOfList("description1", "description")
+        for description in descriptions:
+            self.infoDescriptions.append(InfoDescription(description[0], description[1]))
+
+class InfoDescription:
+    def __init__(self, field, type):
+        self.type = type
+        self.field = field
+        self.split = ''
+
+    def getType(self):
+        return self.type
+
+    def getField(self):
+        return self.field
+
+    def getSplit(self):
+        return self.split
+
+if __name__ == "__main__":
+    coreInfoCollector = CoreInfoCollector("coreInfoInput")
+ #   coreInfoCollector.loadDescription()
+    coreInfoCollector.loadDescriptionFromFIle("coreInfo.cfg")
+    coreInfoCollector.collect()
